6a4d1fa71b334adcc0f40c4661dac0ab6b2c8194
sharma-archit <141180499+sharma-archit@users.noreply.github.com>
2023-08-07 22:16:41 -0600

    Add files via upload

---
+++ simpleCPU/simpleCPU.srcs/sources_1/new/clkDivider.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/07/2023 05:04:23 PM
+// Design Name: 
+// Module Name: clkDivider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clkDivider(
+
+    );
+endmodule
