a79d800e903a3100ebc5fa9c5f4b153dda29d36a
sharma-archit <141180499+sharma-archit@users.noreply.github.com>
2023-08-28 13:07:48 -0600

    Update priority_encoder.v

--- 3to2PriorityEncoder/priority encoder 3 to 2.srcs/sources_1/new/priority_encoder.v
+++ 3to2PriorityEncoder/priority encoder 3 to 2.srcs/sources_1/new/priority_encoder.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Archit Sharma
 // 
 // Create Date: 04/03/2023 12:27:19 PM
 // Design Name: 
