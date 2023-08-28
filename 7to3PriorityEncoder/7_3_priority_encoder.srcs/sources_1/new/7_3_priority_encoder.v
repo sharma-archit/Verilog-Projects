d43dd98784934a7dccfbb63ae1c1773aa12af537
sharma-archit <141180499+sharma-archit@users.noreply.github.com>
2023-08-28 13:09:21 -0600

    Update 7_3_priority_encoder.v

--- 7to3PriorityEncoder/7_3_priority_encoder.srcs/sources_1/new/7_3_priority_encoder.v
+++ 7to3PriorityEncoder/7_3_priority_encoder.srcs/sources_1/new/7_3_priority_encoder.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Archit Sharma
 // 
 // Create Date: 04/05/2023 11:19:43 AM
 // Design Name: 
